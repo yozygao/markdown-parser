3d9afec94c14f1872a46ada685cfbc5058b1cb45
yozygao <yozygao@gmail.com>
2022-04-15 15:24:42 -0700

    Part 5 test 2
    Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
            at java.base/java.lang.StringLatin1.newString(StringLatin1.java:715)
            at java.base/java.lang.String.substring(String.java:1879)
            at MarkdownParse.getLinks(MarkdownParse.java:23)
            at MarkdownParse.main(MarkdownParse.java:38)

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,17 +12,21 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            if(openBracket == -1){
-                break;
+            //if(markdown.contains("[")&&markdown.contains("]")&&markdown.contains("(")&&markdown.contains(")")){
+                int openBracket = markdown.indexOf("[", currentIndex);
+                int closeBracket = markdown.indexOf("]", openBracket);
+                int openParen = markdown.indexOf("(", closeBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                /*if(openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1){
+                    break;
+                }*/
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
             }
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
-
-        }
+            //else{
+                //break;
+            //}
+        //}
 
         return toReturn;
     }
