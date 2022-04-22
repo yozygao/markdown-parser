f4ca0444b78d2db6c162c3cd43a63b550537e14c
yozygao <yozygao@gmail.com>
2022-04-22 14:40:57 -0700

    Create MarkdownParseTest.java
    added JUnit and tests

---
+++ MarkdownParseTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.*;
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+}
