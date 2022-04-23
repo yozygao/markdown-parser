b1de36cdfe51eb2f14d095e351bc2ba2f4e0a4f1
yozygao <yozygao@gmail.com>
2022-04-23 13:32:50 -0700

    Reset

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -44,3 +44,48 @@ public class MarkdownParseTest {
         assertEquals(links, MarkdownParse.getLinks(content));
     }
 }
+
+/**
+ * public class MarkdownParse {
+
+    public static ArrayList<String> getLinks(String markdown) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        // find the next [, then find the ], then find the (, then read link upto next )
+        int currentIndex = 0;
+        while(currentIndex < markdown.length()) {
+            if(markdown.contains("[")&&markdown.contains("]")&&markdown.contains("(")&&markdown.contains(")")){
+                int openBracket = markdown.indexOf("[", currentIndex);
+                int closeBracket = markdown.indexOf("]", openBracket);
+                int openParen = markdown.indexOf("(", closeBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                if(openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1){
+                    break;
+                }
+                else if(openBracket != 0 &&markdown.substring(openBracket-1, openBracket).equals("!")){
+                    currentIndex = closeParen + 1;
+                }
+                else if(openParen == closeBracket + 1){
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                    currentIndex = closeParen + 1;
+                }
+                else{
+                    currentIndex = closeParen + 1;
+                }
+            }
+            else{
+                break;
+            }
+        }
+
+        return toReturn;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+	    System.out.println(links);
+    }
+}
+ */
